97961b3260ae0965ad0986ca07a728040bd99e27
nlegay <erbenetnl@gmail.com>
2016-03-06 14:41:18 +0100

    methode directe

--- src/Course.java
+++ src/Course.java
@@ -1,10 +1,12 @@
 import java.util.Random;
 
 public class Course {
-
+	
 	public static void main(String[] args) {
-		int x=0, y=0;
 		Random r = new Random();
+		int choix;
+		int[] x = {0,0};
+		int[] y = {0,1};
 		String[] gifs={"images/un.gif", "images/deux.gif", "images/trois.gif", "images/quatre.gif"};
 		int taille = 10;
 		int[][] positions = new int[taille][taille];
@@ -13,27 +15,44 @@ public class Course {
 		for (int i=0;i<taille;i++)
 			for (int j=0;j<taille;j++)
 			positions[i][j]=0;
-		positions[x][y] = 1;
+		positions[x[0]][y[0]] = 1;
+		positions[x[1]][y[1]] = 2;
 		plateau.setJeu(positions);
 		plateau.affichage();
 		
-		while(positions[taille-1][taille-1] != 1){
-			int choix = r.nextInt(3);
+		while(positions[taille-1][taille-1] == 0){
+			try{Thread.sleep(500);}catch(Exception ie){}
+			choix = r.nextInt(3);
 			if(choix==0){
-				if(plateau.deplacer(x, y, x+1, y)){
-					x++;
+				if(plateau.deplacer(x[0], y[0], x[0]+1, y[0])){
+					x[0]++;
 				}
 			}else if(choix==1){
-				if(plateau.deplacer(x, y, x, y+1)){
-					y++;
+				if(plateau.deplacer(x[0], y[0], x[0], y[0]+1)){
+					y[0]++;
 				}
 			}else if(choix==2){
-				if(plateau.deplacer(x, y, x+1, y+1)){
-					x++;
-					y++;
+				if(plateau.deplacer(x[0], y[0], x[0]+1, y[0]+1)){
+					x[0]++;
+					y[0]++;
 				}
 			}
-			try{Thread.sleep(1000);}catch(Exception ie){}
+			try{Thread.sleep(500);}catch(Exception ie){}
+			choix = r.nextInt(3);
+			if(choix==0){
+				if(plateau.deplacer(x[1], y[1], x[1]+1, y[1])){
+					x[1]++;
+				}
+			}else if(choix==1){
+				if(plateau.deplacer(x[1], y[1], x[1], y[1]+1)){
+					y[1]++;
+				}
+			}else if(choix==2){
+				if(plateau.deplacer(x[1], y[1], x[1]+1, y[1]+1)){
+					x[1]++;
+					y[1]++;
+				}
+			}
 			plateau.affichage();
 		}
 		System.out.println("Bravo!");
